cc1bc589119638f755cc804b65a04aab4d0be50e
Sven Keunen <svenkeunen@hotmail.com>
2023-12-29 11:17:58 +0100

    refactor: Improve readability and idiomatic Rust in day1 function
    - Use `const` for FILE_PATH
    - Provide a meaningful error message when reading the file
    - Use the `lines` iterator for iterating over lines
    - Iterate over characters directly to avoid unnecessary allocations
    - Use `match` for pattern matching when converting characters to digits
    - Directly convert values to a formatted string, avoiding unnecessary conversions
    - Use `+=` for sum calculation for better readability

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,4 @@
-
-
-const FILE_PATH: &'static str = "src/input_day1.txt";
+const FILE_PATH: &str = "src/input_day1.txt";
 
 fn main() {
     day1();
@@ -9,46 +7,40 @@ fn main() {
 pub fn day1(){
     println!("Day1!");
 
-    let input_res = std::fs::read_to_string(FILE_PATH);
-    if input_res.is_err(){
-        println!("Error unwrapping result");
-        return;
-    }
+    let input_res = std::fs::read_to_string(FILE_PATH).expect("Failed to read file");
 
-    let input = input_res.unwrap();
-    let split_lines = input.split("\n");
     let mut sum = 0;
 
-    for split_line in split_lines{
+    for split_line in input_res.lines(){
         println!("{}", split_line);
-        let char_vec: Vec<char> = split_line.chars().collect();
 
         let mut val1_set = false; //Added this bool because couldn't use -1 as 'uninit val'
         let mut val1 = 0;
         let mut val2 = 0;
 
-        for char_val in char_vec{
+        for char_val in split_line.chars(){
             let val_from_char = char_val.to_digit(10); //string uses .parse(), char .to_digit()
 
-            if val_from_char.is_none(){
-                continue;
-            }
+            match val_from_char{
+                Some(val) => {
+                    if !val1_set{
+                        val1 = val;
+                        val1_set = true;
+                    }
 
-            if !val1_set{
-                val1 = val_from_char.unwrap();
-                val1_set = true;
+                    val2 = val;
+                }
+                None => continue,
             }
-
-            val2 = val_from_char.unwrap();
         }
+
         println!("Vals found {} {}", val1, val2);
         //Concat the vals and add them to sum
-        let mut vals_string =String::from("");
-        vals_string.push(char::from_digit(val1,10).unwrap());
-        vals_string.push(char::from_digit(val2,10).unwrap());
-        let vals_string_int:i32 = vals_string.parse().unwrap();
+        let vals_string = format!("{}{}", val1, val2);
+        let vals_string_int: i32 = vals_string.parse().unwrap();
         println!("{}", vals_string_int);
-        sum = sum + vals_string_int;
+
+        sum += vals_string_int;
     }
 
     println!("Total sum: {}", sum);
